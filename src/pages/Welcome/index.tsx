05872d943401d48a23f78c4b9b808e97248ff9bd
thumbsu <me@thumbsu.dev>
2020-08-14 18:45:21 +0900

    ADD welcome background image

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,15 +1,18 @@
-import { Button, SafeAreaView, Text, View } from 'react-native'
+import { Button, ImageBackground, SafeAreaView, Text } from 'react-native'
 
 import React from 'react'
+import styles from './styles'
 import { useNavigation } from '@react-navigation/native'
 
 export default function Welcome() {
   const navigation = useNavigation()
 
   return (
-    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={require('../../assets/img/whale.jpg')} style={styles.image}>
+        <Text style={styles.text}>Home Screen</Text>
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      </ImageBackground>
     </SafeAreaView>
   )
 }
---
+++ src/pages/Welcome/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'grey',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+})
